0116be182c35d950efae99eb2c7120a80906ad49
georgi <georgi.spirov@stanga.net>
2025-10-04 16:12:02 +0300

    week 2

---
+++ Week2/Explore2.15/src/constructors/Book.java
@@ -0,0 +1,20 @@
+package constructors;
+
+public class Book {
+    private final String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                '}';
+    }
+}
---
+++ Week2/Explore2.15/src/constructors/Main.java
@@ -0,0 +1,8 @@
+package constructors;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book = new Book("Book1");
+        System.out.println(book);
+    }
+}
